3eb04a286942d2b102e913365a8230dae9d9f027
CODIFIEST <surfdean808@gmail.com>
2023-02-19 00:04:33 -0800

    update colors for legible input boxes

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -20,11 +20,9 @@
     <div class="hero-overlay bg-opacity-60" />
     <div class="hero-content text-center">
       <div class="max-w-md">
-        <h1 class="mb-5 text-5xl font-bold text-secondary">Hello there</h1>
-        <p class="mb-5 text-secondary">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-          id nisi.
+        <h1 class="mb-5 text-5xl font-bold text-primary">Hello there</h1>
+        <p class="mb-5 text-primary">
+          Pandas' last resort was the super secret kill switch embedded in the crane core logic.
         </p>
         <div class="container space-y-4">
           <div class="grid grid-cols-2 gap-4">
